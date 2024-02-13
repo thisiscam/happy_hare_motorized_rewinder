0d0b37c32cd687e378e405521097ee5324446565
Cambridge Yang <jq_yang@mit.edu>
2024-02-13 01:11:33 -0500

    ble implementation

--- drv8833_motor.py
+++ drv8833_motor.py
@@ -8,147 +8,37 @@ import math
 
 MOTOR_MIN_TIME = 0.100
 
+
 class Drv8833Motor:
-    def __init__(self, config, default_shutdown_speed=0.):
-        self.printer = config.get_printer()
-        self.last_speed_a = 0.
-        self.last_speed_b = 0.
-        self.last_decay_mode = 'fast'
-        self.last_motor_time = 0.
-        # Read config
-        self.max_power = config.getfloat('max_power', 1., above=0., maxval=1.)
-        self.kick_start_speed = config.getfloat('kick_start_speed', self.max_power, above=0., maxval=1.)
-        self.kick_start_time = config.getfloat('kick_start_time', 0.1, minval=0.)
-        cycle_time = config.getfloat('cycle_time', 0.010, above=0.)
-        hardware_pwm = config.getboolean('hardware_pwm', False)
-        shutdown_speed = config.getfloat(
-            'shutdown_speed', default_shutdown_speed, minval=0., maxval=1.)
-        # Setup pwm object
-        ppins = self.printer.lookup_object('pins')
-        self.mcu_motor_a = ppins.setup_pin('pwm', config.get('pin_a'))
-        self.mcu_motor_b = ppins.setup_pin('pwm', config.get('pin_b'))
-        self.mcu_motor_a.setup_max_duration(0.)
-        self.mcu_motor_b.setup_max_duration(0.)
-        self.mcu_motor_a.setup_cycle_time(cycle_time, hardware_pwm)
-        self.mcu_motor_b.setup_cycle_time(cycle_time, hardware_pwm)
-        shutdown_power = max(0., min(self.max_power, shutdown_speed))
-        self.mcu_motor_a.setup_start_value(0., shutdown_power)
-        self.mcu_motor_b.setup_start_value(0., shutdown_power)
 
-        # Register callbacks
-        self.printer.register_event_handler("gcode:request_restart",
-                                            self._handle_request_restart)
-        self.mcu.register_config_callback(self.build_config)
+  def __init__(self, config):
+    self.printer = config.get_printer()
+    self.pin_a = config.getint('pin_a')
+    self.pin_b = config.getint('pin_b')
+    self.pwm_freq = config.getint('pwm_freq', 5000)
+    self.rewind_fast_speed = config.getfloat('rewind_fast_speed', 1.0)
+    self.rewind_slow_speed = config.getfloat('rewind_slow_speed', 0.4)
+    self.load_fast_speed = config.getfloat('load_fast_speed', 0.4)
+    self.load_slow_speed = config.getfloat('load_slow_speed', 0.2)
+    self.startup_speed = config.getfloat('startup_speed', 1.0)
+    self.startup_duration = config.getfloat('startup_duration', 0.1)
+    self.soft_start_duration = config.getfloat('soft_start_duration', 0.)
+    self.ble_device_idx = config.getint('ble_device_idx', 0)
 
-        gcode = self.printer.lookup_object("gcode")
-        self.motor_name = config.get_name().split()[-1]
-        gcode.register_mux_command("SET_MOTOR_VELOCITY", "MOTOR",
-                                   self.motor_name,
-                                   self.cmd_SET_MOTOR_VELOCITY,
-                                   desc=self.cmd_SET_MOTOR_VELOCITY_help)
+    rewinder_idx = config.get_name().split()[1].split('rewinder')
+    self.gcode = self.printer.lookup_object('gcode')
+    self.gcode.register_mux_command('REWIND_CONTROL',
+                                    'ID',
+                                    rewinder_idx,
+                                    self.cmd_REWIND_CONTROL,
+                                    desc=self.cmd_REWIND_CONTROL.__doc__)
+    self.mmu_patch = self.printer.lookup_object('mmu_rewinder_patch')
 
-    @property
-    def mcu(self):
-        return self.mcu_motor_a.get_mcu()
-    
-    @property
-    def _sx1509(self):
-        return self.mcu_motor_a._sx1509
-    
-    @property
-    def _i2c(self):
-        return self._sx1509._i2c
+  def cmd_REWIND_CONTROL(self, gcmd):
+    """Control the rewind motor"""
+    mode = gcmd.get('MODE')
+    self.mmu_patch._rewind_control_send(self, mode)
 
-    def build_config(self):
-        if not hasattr(self._i2c, "i2c_write_batch_cmd"):
-          self._i2c.i2c_write_batch_cmd = self.mcu.lookup_command(
-              "i2c_write_batch oid=%c chunks=%*s data=%*s", cq=self._i2c.cmd_queue)
-
-    def set_speed(self, print_time, velocity, decay_mode):
-        velocity = math.copysign(max(0., min(self.max_power, abs(velocity) * self.max_power)), velocity)
-        if velocity == self.last_motor_velocity and decay_mode == self.last_decay_mode:
-            return
-        speed = abs(velocity)
-        if decay_mode == 'fast':
-            speed_a, speed_b = speed, 0
-            ks_a, ks_b = self.kick_start_speed, 0
-        else:
-            speed_a, speed_b = 1, 1 - speed
-            ks_a, ks_b = 1, 1 - self.kick_start_speed
-        if velocity < 0:
-            speed_a, speed_b = speed_b, speed_a
-            ks_a, ks_b = ks_b, ks_a
-
-        print_time = max(self.last_motor_time + MOTOR_MIN_TIME, print_time)
-        if (speed and self.kick_start_time and abs(velocity - self.last_motor_velocity) > .5):
-            self.set_ab_speed(print_time, ks_a, ks_b)
-            print_time += self.kick_start_time
-        self.set_ab_speed(print_time, speed_a, speed_b)
-        self.last_motor_time = print_time
-        self.last_decay_mode = decay_mode
-    
-    def ab_to_velocity(self, a, b):
-        if b == 0:
-            return a
-        elif a == 0:
-            return -b
-        elif b == 1:
-            return a - 1
-        elif a == 1:
-            return 1 - b
-        else:
-            return None
-    
-    @property
-    def last_motor_velocity(self):
-        return self.ab_to_velocity(self.last_speed_a, self.last_speed_b)
-    
-    def set_ab_speed(self, print_time, a, b):
-        self._set_pwm(self.mcu_motor_b, b)
-        self._set_pwm(self.mcu_motor_a, a)
-        self._send_registers(print_time)
-        self.last_speed_a = a
-        self.last_speed_b = b
-
-    def _set_pwm(self, motor, value):
-        self._sx1509.set_register(motor._i_on_reg, ~int(255 * value)
-                                  if not motor._invert
-                                  else int(255 * value) & 0xFF)
-    
-    def _send_registers(self, print_time):
-        regs = [m._i_on_reg for m in (self.mcu_motor_a, self.mcu_motor_b)]
-        data = []
-        for reg in regs:
-          data += [reg & 0xFF]
-          if reg in self._sx1509.reg_dict:
-              # Word
-              data += [(self._sx1509.reg_dict[reg] >> 8) & 0xFF,
-                       self._sx1509.reg_dict[reg] & 0xFF]
-          elif reg in self._sx1509.reg_i_on_dict:
-              # Byte
-              data += [self._sx1509.reg_i_on_dict[reg] & 0xFF]
-        clock = self.mcu.print_time_to_clock(print_time)
-        self._i2c.i2c_write_batch_cmd.send([self._i2c.oid, [2, 2], data], 
-                minclock=self._sx1509._last_clock, reqclock=clock)
-        self._sx1509._last_clock = clock
-
-
-    cmd_SET_MOTOR_VELOCITY_help = "Sets the velocity of a DC motor"
-    def cmd_SET_MOTOR_VELOCITY(self, gcmd):
-        velocity = gcmd.get_float('VELOCITY', 0.)
-        decay_mode = gcmd.get('DECAY_MODE', default='fast')
-        toolhead = self.printer.lookup_object('toolhead')
-        toolhead.register_lookahead_callback((lambda pt:
-                                              self.set_speed(pt, velocity, decay_mode)))
-
-    def _handle_request_restart(self, print_time):
-        self.set_speed(print_time, 0., 'fast')
-
-    def get_status(self, eventtime):
-        return {
-            'velocity': self.last_motor_velocity,
-            'decay_mode': self.last_decay_mode,
-        }
 
 def load_config_prefix(config):
-    return Drv8833Motor(config)
+  return Drv8833Motor(config)
--- mmu_rewinder_patch.py
+++ mmu_rewinder_patch.py
@@ -4,7 +4,14 @@
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
 
+#pylint:disable=protected-access
+
 import contextlib
+import bleak
+import asyncio
+import threading
+import configfile
+import struct
 
 
 @contextlib.contextmanager
@@ -21,18 +28,24 @@ def patch_method(obj, method_name, new_method):
     setattr(obj, method_name, old_method)
 
 
-_mmu_unload_bowden = None
-_mmu_unload_gate = None
-_mmu_load_bowden = None
 _mmu_trace_filament_move = None
 
 
 class MmuRewinderPatch:
+  """Patch the MMU to control the rewind motors"""
 
   def __init__(self, config):
     self.printer = config.get_printer()
     self.mmu = self.printer.lookup_object('mmu')
     self.gcode = self.printer.lookup_object('gcode')
+    self.printer.register_event_handler('klippy:connect', self.handle_connect)
+    self.printer.register_event_handler("klippy:disconnect",
+                                        self.handle_disconnect)
+    self.ble_loop = None
+    self.ble_addresses = config.get('ble_addresses', []).split(',')
+    self.ble_devices = []
+    self.ble_timeout = config.getfloat('ble_timeout', 10.0)
+
     self._patch_mmu()
 
   def _patch_mmu(self):
@@ -40,19 +53,67 @@ class MmuRewinderPatch:
     _mmu_trace_filament_move = self.mmu._trace_filament_move
     self.mmu._trace_filament_move = self._trace_filament_move
 
+  def _setup_ble_thread(self):
+    """Setup a new thread for the BLE event loop. All BLE commands will be run on this thread."""
+    if self.ble_loop is None:
+      self.ble_loop = asyncio.new_event_loop()
+
+      def _init(loop):
+        asyncio.set_event_loop(loop)
+        loop.run_forever()
+
+      threading.Thread(target=_init, args=(self.ble_loop,)).start()
+
+  def handle_connect(self):
+    self._setup_ble_thread()
+    asyncio.run(self._connect_bles())
+
+  async def _connect_bles(self):
+
+    def handle_ble_disconnect(self, client, *args, **kwargs):
+      self.mmu._mmu_pause(f"BLE device {client.address} disconnected")
+
+    for ble_address in self.ble_addresses:
+      client = await bleak.BleakClient(
+          ble_address,
+          timeout=self.ble_timeout,
+          disconnected_callback=self.handle_ble_disconnect)
+      if not client:
+        raise configfile.error(
+            f"Unable to connect to BLE device at {ble_address}")
+      self.mmu._log_info(f"Connected to BLE device at {ble_address}")
+      self.ble_devices.append(client)
+
+  async def _disconnect_bles(self):
+    for client in self.ble_devices:
+      await client.disconnect()
+
+  def handle_disconnect(self):
+    self.run_on_ble_thread(self._disconnect_bles())
+
+  def run_on_ble_thread(self, coro):
+    """Run a coroutine on the BLE thread"""
+    assert self.ble_loop is not None, "BLE thread not running"
+    return asyncio.run_coroutine_threadsafe(coro, self.ble_loop)
+
   def _trace_filament_move(self, trace_str, *args, **kwargs):
     if trace_str == "Course unloading move from bowden":
       self.rewind_control("rewind_fast")
       ret = _mmu_trace_filament_move(trace_str, *args, **kwargs)
       self.rewind_control("stop")
-    elif trace_str in ("Reverse homing to extruder sensor", "Reverse homing to toolhead sensor", "Bowden pre-unload test", "Reverse homing to gate sensor", "Unloading extruder", "Final parking", "Bowden pre-unload test"):  # TODO: handle case without gate sensor
+    elif trace_str in (
+        "Reverse homing to extruder sensor",
+        "Reverse homing to toolhead sensor", "Bowden pre-unload test",
+        "Reverse homing to gate sensor", "Unloading extruder", "Final parking",
+        "Bowden pre-unload test"):  # TODO: handle case without gate sensor
       self.rewind_control("rewind_slow")
       ret = _mmu_trace_filament_move(trace_str, *args, **kwargs)
       self.rewind_control("stop")
       if trace_str == "Final parking":
-          self.mmu._movequeues_wait_moves()
+        self.mmu._movequeues_wait_moves()
     elif trace_str == "Course loading move into bowden":
-      if self.mmu.gate_selected >= 0 and self.mmu.gate_status[self.mmu.gate_selected] != self.mmu.GATE_AVAILABLE_FROM_BUFFER:
+      if self.mmu.gate_selected >= 0 and self.mmu.gate_status[
+          self.mmu.gate_selected] != self.mmu.GATE_AVAILABLE_FROM_BUFFER:
         self.rewind_control("load_slow")
         ret = _mmu_trace_filament_move(trace_str, *args, **kwargs)
         self.rewind_control("stop")
@@ -67,9 +128,58 @@ class MmuRewinderPatch:
   def rewind_control(self, mode):
     """Control the rewind motor"""
     rewinder_idx = self.mmu.gate_selected
-    if type(rewinder_idx) is int:
-      self.gcode.run_script_from_command("REWINDER_CONTROL ID=%d MODE=%s" %
-                                         (rewinder_idx, mode))
+    if rewinder_idx >= 0:
+      rewinder = self.printer.lookup_object(
+          f"drv8833_motor rewinder{rewinder_idx}")
+      self._rewind_control_send(rewinder, mode)
+
+  def _rewind_control_send(self, rewinder, mode):
+    """Send the rewind control command to the BLE device"""
+    decay_mode = 0  # default slow decay mode
+    if mode == "stop":
+      speed = 0
+      decay_mode = 1
+    elif mode == "brake":
+      speed = 0
+    elif mode == "rewind_fast":
+      speed = -rewinder.rewind_fast_speed
+    elif mode == "rewind_slow":
+      speed = -rewinder.rewind_slow_speed
+    elif mode == "load_fast":
+      speed = rewinder.load_fast_speed
+    elif mode == "load_slow":
+      speed = rewinder.load_slow_speed
+    else:
+      raise ValueError(f"Invalid rewind mode {mode}")
+
+    client = self.ble_devices[rewinder.ble_device_idx]
+    self.run_on_ble_thread(
+        send_command(client, rewinder.pin_a, rewinder.pin_b, decay_mode, speed,
+                     rewinder.pwm_freq, rewinder.startup_speed,
+                     rewinder.startup_duration, rewinder.soft_start_duration,
+                     False))
+
+
+UART_SERVICE_UUID = "fb1e4001-54ae-4a28-9f74-dfccb248601d"
+UART_RX_CHAR_UUID = "fb1e4002-54ae-4a28-9f74-dfccb248601d"
+UART_TX_CHAR_UUID = "fb1e4003-54ae-4a28-9f74-dfccb248601d"
+
+
+async def send_command(client,
+                       pin_a,
+                       pin_b,
+                       decay_mode=0,
+                       speed=0,
+                       pwm_freq=5000,
+                       startup_speed=0,
+                       startup_duration=0,
+                       soft_start_duration=0,
+                       response: bool = False):
+  data = struct.pack('<BBBBbbIff', 0xEE, pin_a, pin_b, decay_mode, speed,
+                     startup_speed, pwm_freq, startup_duration,
+                     soft_start_duration)
+  return await client.write_gatt_char(UART_RX_CHAR_UUID, bytearray(data),
+                                      response)
 
 
 def load_config(config):
